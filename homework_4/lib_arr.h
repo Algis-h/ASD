247510e3305b05ef53b8d94f412a1a16d16ce4ac
Algis <algis.h@yandex.ru>
2021-03-21 22:07:27 +0300

    Выполнена 4 домашняя работа.

---
+++ homework_4/lib_arr.h
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void printArray(int, int, int*);
+void swap(int*, int*);
+void reverse(int, int *);
+
+
+void printArray(int x, int y, int *arr)
+{
+	for (int i = 0; i < x; ++i)
+	{
+		for (int j = 0; j < y; ++j)
+		{
+			printf("%d\t", arr[i * x + j]);
+
+		}
+	printf("\n");
+	}
+
+}
+void swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+
+}
+
+void reverse(int size, int *arr)
+{
+	for (int i = 0; i < size/2; i++)
+	{
+		swap(&arr[i], &arr[size - i - 1]);
+	}
+}
